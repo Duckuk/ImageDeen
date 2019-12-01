6deffbcc4ad7148c19a6389b8b275514f74babd0
Duckuk <36255460+Duckuk@users.noreply.github.com>
2019-12-01 10:42:32 +1030

    Add check for unknown modes

--- ImageDeen/imagedeen.cpp
+++ ImageDeen/imagedeen.cpp
@@ -246,6 +246,9 @@ int main(int argc, char *argv[]) {
 		case 'c':
 			decodeImageLegacy(image);
 			break;
+		default:
+			cerr << "Error: Unknown mode";
+			break;
 	}
 	this_thread::sleep_for(std::chrono::seconds(2));
 	return 1;
